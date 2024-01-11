9a8c0114466f28c2f775617529139db126e348ae
Someone68 <97077158+Someone68@users.noreply.github.com>
2024-01-11 02:34:26 +0000

    backend added
    Co-authored-by: TheMangoNerd <TheMangoNerd@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 6942;
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.get("/", (req, res) => {
+  response.sendStatus(200);
+});
+
+// Middleware to remove .html extension from URLs
+app.use((req, res, next) => {
+  const filePath = path.join(__dirname, "public", `${req.path}`);
+  if (req.path !== "/" && fs.existsSync(`${filePath}.html`)) {
+    req.url = `${req.path}.html`;
+  }
+  next();
+});
+
+// Route to serve .html files
+app.get("/*.html", (req, res, next) => {
+  const filePath = path.join(__dirname, "public", `${req.path}`);
+  if (fs.existsSync(`${filePath}`)) {
+    res.sendFile(`${filePath}`);
+  } else {
+    next();
+  }
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port 3000`);
+});
